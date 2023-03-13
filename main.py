a55823376f81e98813203bd6ba339fe888335d7d
vjnnxx <florianovinicius09@gmail.com>
2023-03-13 11:43:51 -0300

    grafico de audio em tempo real funcionando

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#teste do botao de gravar
+
+import queue
+import sounddevice as sd
+from matplotlib.animation import FuncAnimation
+
+
 #Classe para guardar informações do arquivo .wav
 class arquivo:
 
@@ -29,9 +36,9 @@ class arquivo:
 
         fig = plt.figure()
 
-        plt.plot(time,self.audioBuffer)
+        plt.plot(time,self.audioBuffer/10000)
         plt.xlabel('Tempo [s]')
-        plt.ylabel('Amplitude')
+        plt.ylabel('Amplitude [V]')
         plt.title(name)
         plt.show()
 
@@ -67,6 +74,93 @@ def browseFiles():
 def showFigures():
     print('qualquer coisa')
 
+def recordAudio():
+    print('Botão para gravar audio')
+    device = 0 # id of the audio device by default
+    window = 1000 # window for the data
+    downsample = 4 # how much samples to drop (inicialmente era 1)
+    channels = [1] # a list of audio channels
+    interval = 30 # this is update interval in miliseconds for plot
+
+    # lets make a queue
+    q = queue.Queue()
+    
+    device_info =  sd.query_devices(device, 'input')
+    samplerate = device_info['default_samplerate']
+    length  = int(window*samplerate/(1000*downsample))
+
+   
+    print("Sample Rate: ", samplerate)
+
+    
+
+    # Now we require a variable to hold the samples 
+
+    plotdata =  np.zeros((length,len(channels)))
+    # Lets look at the shape of this plotdata 
+    print("plotdata shape: ", plotdata.shape)
+    # So its vector of length 44100
+    # Or we can also say that its a matrix of rows 44100 and cols 1
+
+    # next is to make fig and axis of matplotlib plt
+    fig,ax = plt.subplots(figsize=(8,4))
+
+    # lets set the title
+    ax.set_title("Entrada de áudio")
+
+    # Make a matplotlib.lines.Line2D plot item of color green
+    # R,G,B = 0,1,0.29
+
+    lines = ax.plot(plotdata,color = (0,1,0.29))
+
+    # We will use an audio call back function to put the data in queue
+
+    def audio_callback(indata,frames,time,status):
+        q.put(indata[::downsample,[0]])
+
+    # now we will use an another function 
+    # It will take frame of audio samples from the queue and update
+    # to the lines
+
+    def update_plot(frame):
+        nonlocal plotdata
+        while True:
+            try: 
+                data = q.get_nowait()
+            except queue.Empty:
+                break
+            shift = len(data)
+            plotdata = np.roll(plotdata, -shift,axis = 0)
+            # Elements that roll beyond the last position are 
+            # re-introduced 
+            plotdata[-shift:,:] = data
+            
+        for column, line in enumerate(lines):
+            line.set_ydata(plotdata[:,column])
+        return lines
+    ax.set_facecolor((0,0,0))
+    # Lets add the grid
+    #ax.set_yticks([1])
+    ax.yaxis.grid(True)
+
+    """ INPUT FROM MIC """
+
+    stream  = sd.InputStream( device = device, channels = max(channels), samplerate = samplerate, callback  = audio_callback)
+
+
+    """ OUTPUT """		
+
+
+    plt.ylim(top=1)
+    
+
+    ani  = FuncAnimation(fig,update_plot, interval=interval,blit=True)
+
+    plt.ylabel("Amplitude")
+    with stream:
+        plt.show()
+
+
 root = Tk()
 
 root.maxsize(900,600)
@@ -85,6 +179,8 @@ button_explore = Button(root, text = "Abrir arquivo", command = browseFiles)
 
 button_show = Button(root, text='Exibir figuras salvas', command= showFigures)
 
+button_record = Button(root, text='Gravar áudio', command=recordAudio)
+
 button_exit = Button(root, text="Sair", command=exit)
 
 
@@ -96,7 +192,9 @@ button_explore.grid(column = 0, row = 2)
   
 button_show.grid(column=0, row=3)
 
-button_exit.grid(column = 0,row = 4)
+button_record.grid(column=0, row=4)
+
+button_exit.grid(column = 0,row = 5)
 
 
 root.mainloop()
