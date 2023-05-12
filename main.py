8a90d93c4665dcddfbe8d3b3ad1a2878f45c9331
vjnnxx <florianovinicius09@gmail.com>
2023-05-11 21:39:16 -0300

    començando desenvolvimento com pyside

--- main.py
+++ main.py
@@ -1,86 +1,124 @@
-from tkinter import *
-from tkinter import ttk
+from qdarktheme import load_stylesheet
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont, QAction
+from PySide6.QtWidgets import ( 
+    QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QMainWindow, QFileDialog)
 
+from modules.functions import arquivo
 
-import threading
-import sys 
-sys.path.append('./modules')
-from modules.functions import *
+import numpy as np
 
-root = Tk()
+import os
+import scipy.io
+import scipy.io.wavfile
 
-#Configurações do menu superior
-menu = Menu(root)
-
-root.config(menu=menu)
-
-menuArquivo = Menu(menu)
-menu.add_cascade(label="Arquivo", menu=menuArquivo)
-menuArquivo.add_command(label="Abrir")
-
-menuSalvar = Menu(menu)
-menu.add_cascade(label="Salvar", menu=menuSalvar)
-menuSalvar.add_command(label="Salvar como...")
-
-menuAnalises = Menu(menu)
-menu.add_cascade(label="Análises", menu=menuAnalises)
-menuAnalises.add_command(label="Nova análise")
-menuAnalises.add_command(label="Gerenciar análises")
-
-menuConfig = Menu(menu)
-menu.add_cascade(label="Configurações", menu=menuConfig)
-menuConfig.add_command(label="Entrada")
-menuConfig.add_command(label="Gráfico")
-
-root.maxsize(900,600)
-
-root.geometry("300x200")
-
-root.resizable(0,0)
-
-root.title("EMG")
-
-mainframe = ttk.Frame(root)
-
-
-aux = ''
-
-button_explore = Button(root, text = "Abrir arquivo", command = browseFiles)
-button_exit = Button(root, text="Sair", command= lambda: [stop(False), exit()])
+import matplotlib.pyplot as plt
+import matplotlib
 
 
 
 
-
-
-thread1 = ''
-thread2 = ''
-thread_timer = ''
-
-
-button_record = Button(root, text= "Gravar", command=recordAudio)
-
-
-button_record.grid(column=0, row=0)
-
-button_explore.grid(column=1, row=0)
-  
-
-button_exit.grid(column=2, row=0)
+def printar():
+    print('Botão foi clicado!')
 
 
 
-
-#record(root)
-#recordAudio(root)
-
+#Janela principal do app
+class Window(QMainWindow):
 
 
-root.rowconfigure(0, weight=4)
-root.rowconfigure(1, weight=1)
+    def abrir_arquivo(self):
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        dialog.setNameFilter("Audio (*.wav)")
+        dialog.setViewMode(QFileDialog.ViewMode.List)
+        if dialog.exec():
+            filename = dialog.selectedFiles()
+
+            filename = filename[0]
+
+            print(filename)
+
+            file = arquivo()
+
+            name = filename.split('/')
+            name = name[-1]
+
+            file = arquivo()
+
+            file.path = filename
+
+            dataset_path = os.path.join(file.path) 
+            file.wavedata = os.path.join(dataset_path)
+
+            file.sampleRate, file.audioBuffer = scipy.io.wavfile.read(file.wavedata)
+
+            duracao = len(file.audioBuffer)/file.sampleRate
+
+
+            tempo = np.arange(0,duracao,1/file.sampleRate)
+            
+            fig = plt.figure(figsize=(5,5), dpi=100)
+
+            a = fig.add_subplot(111)
+
+            a.plot(tempo, file.audioBuffer/10000)
+
+            plt.xlabel('Tempo [s]')
+            plt.ylabel('Amplitude [Hz]')
+            plt.title(name)
+
+
+            self.wid = QWidget()
+            self.wid.resize(250, 150)
+            self.wid.setWindowTitle('Arquivo Externo')
+            self.wid.show()
+
+            plt.show()
 
 
 
-root.mainloop()
+    def __init__(self):
+        super().__init__()
+
+        base = QWidget()
+        layout = QVBoxLayout()
 
 
+        #Criando barra de menu
+        
+        action = QAction('Ação', self)
+        action.triggered.connect(printar)
+        menu = self.menuBar()
+        file_menu = menu.addMenu('Menu')
+        file_menu.addAction(action)
+
+
+        #Criando fonte e aplicando configurações
+        font = QFont()
+        font.setPixelSize(90)
+
+
+        #Criando label
+        self.label = QLabel('Salve Crias!')
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.label)
+
+        botaoGravar = QPushButton('Abrir arquivo')
+        botaoGravar.setFont(font)
+        botaoGravar.clicked.connect(self.abrir_arquivo)
+        layout.addWidget(botaoGravar)
+
+        base.setLayout(layout)
+        self.setCentralWidget(base)
+
+
+
+app = QApplication()
+app.setStyleSheet(load_stylesheet())
+
+janela = Window()
+janela.show()
+
+app.exec()
